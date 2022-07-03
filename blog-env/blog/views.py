da8389ca715dec87be73c5ea8addf899fd2ab6b5
Vivek Parmar <200040094@iitb.ac.in>
2022-07-03 18:51:34 +0530

    Can update the posts now, mistakes hidden shshh

--- blog-env/blog/views.py
+++ blog-env/blog/views.py
@@ -1,6 +1,6 @@
 #from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 
 
@@ -13,10 +13,15 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
-    context_object_name = 'anything_you_want'
 
 
 class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
