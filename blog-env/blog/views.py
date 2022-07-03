152f873df9517a56f2d71e3f9b53231336becd23
Vivek Parmar <200040094@iitb.ac.in>
2022-07-03 19:17:17 +0530

    Can delete post now, so many mistakes huh

--- blog-env/blog/views.py
+++ blog-env/blog/views.py
@@ -1,6 +1,8 @@
 #from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Post
 
 
@@ -25,3 +27,9 @@ class BlogUpdateView(UpdateView):
     model = Post
     fields = ['title', 'body']
     template_name = 'post_edit.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
